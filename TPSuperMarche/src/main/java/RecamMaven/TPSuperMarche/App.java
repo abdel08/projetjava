c0d5d614a0dd6edb2d6de06e666c8559cc31a6f8
Utilisateur <Utilisateur@169.254.195.102>
2021-01-28 11:04:42 +0100

    test_envoie

---
+++ TPSuperMarche/src/main/java/RecamMaven/TPSuperMarche/App.java
@@ -0,0 +1,13 @@
+package RecamMaven.TPSuperMarche;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
